eff386e07de1cfd3dafe3bce2c2f44714167529d
richardk88 <richardk88@hotmail.com>
2017-09-12 14:38:00 -0400

    added some style to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,6 +3,23 @@ import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 import axios from 'axios'
 
+const Nav = styled.div`
+    width: 95%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2.5%;
+    background-color: rgba(2, 178, 208, 1);
+    box-shadow: 0px 1px 6px black;
+    a {
+        text-decoration: none;
+        margin: 0 5px;
+        &:visited {
+            color: white;
+        }
+    }
+`
+
 class NavBar extends Component {
     constructor() {
         super();
