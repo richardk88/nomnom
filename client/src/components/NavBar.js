7a989137f7915c526315f3f5605060a9b9de1e05
richardk88 <richardk88@hotmail.com>
2017-09-12 14:37:09 -0400

    imported styled-components to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
 import axios from 'axios'
 
 class NavBar extends Component {
