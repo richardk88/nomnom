e226933284088cc15d11972e62d5fb1275f10d79
richardk88 <richardk88@hotmail.com>
2017-09-12 14:33:35 -0400

    made changes to navbar for signup/signin

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,15 +2,62 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 
 class NavBar extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Nom Nom</h1>
+    constructor() {
+        super();
+        this.state = {
+          user: {},
+          loggedIn: false
+        };
+      }
+    
+      componentWillMount() {
+        this._isLoggedIn();
+      }
+      componentWillReceiveProps() {
+        this._isLoggedIn();
+      }
+    
+      _isLoggedIn = async () => {
+        const response = await axios.get("/auth/validate_token");
+        this.setState({
+          user: response.data.data,
+          loggedIn: response.data.success
+        });
+      };
+      
+      _logOut = async () => {
+        console.log("CLICK");
+        const response = await axios.delete("/auth/sign_out");
+        //Forces refresh of browser
+        window.location.reload();
+      };
+    
+        render(){
+        if (this.state.loggedIn) {
+            return (
+                <Nav>
+                <Link to="/">
+                    <h1>Home</h1>
+                </Link>
                 <div>
-                    <Link to='/signUp'>Sign Up</Link>
-                    <Link to='/signIn'>Sign In</Link>
+                    <span>Signed In As: {this.state.user.email}</span>
+                    <Link to='/new'>Add a new creature</Link>
+                    <a href="#" onClick={this._logOut}> Log Out </a>
                 </div>
+            </Nav>
+                
+            )
+        }
+        return (
+        <Nav>
+            <Link to="/">
+              <h1>Home</h1>
+            </Link>
+            <div>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/signin">Sign In</Link>
             </div>
+        </Nav>
         );
     }
 }
