123906593bd6c76b4a9a6546ddea81e7fae705c5
richardk88 <richardk88@hotmail.com>
2017-09-12 14:23:43 -0400

    added title to navbar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,6 +5,7 @@ class NavBar extends Component {
     render() {
         return (
             <div>
+                <h1>Nom Nom</h1>
                 <Link to='/signUp'>Sign Up</Link>
                 <Link to='/signIn'>Sign In</Link>
             </div>
