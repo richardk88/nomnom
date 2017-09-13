e2483b01d39fef5cbddfcd65476938acab1b6aa1
richardk88 <richardk88@hotmail.com>
2017-09-13 14:44:41 -0400

    created profile edit page

---
+++ client/src/components/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+    render() {
+        return (
+            <div>
+                Profile Edit Page
+            </div>
+        );
+    }
+}
+
+export default ProfileEdit;
